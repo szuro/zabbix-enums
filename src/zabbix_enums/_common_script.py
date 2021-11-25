4138c71bdb4f511b67039de5a0fbda8dfef3f8d9
Robert Szulist <r.szulist@gmail.com>
2021-11-25 20:00:30 +0100

    Add script

---
+++ src/zabbix_enums/_common_script.py
@@ -0,0 +1,17 @@
+from enum import IntEnum
+
+
+class ScriptExecuteOn(IntEnum):
+    AGENT = 0
+    SERVER = 1
+    SERVER_PROXY = 2
+
+
+class ScriptHostAccess(IntEnum):
+    READ = 2
+    WRITE = 3
+
+
+class ScriptType(IntEnum):
+    SCRIPT = 0
+    IPMI = 1

5aeaaa36e6a4d7ec899980acfccebca047dea4b1
Robert Szulist <r.szulist@gmail.com>
2021-11-29 16:41:16 +0100

    Add trigger prototype

---
+++ src/zabbix_enums/common/trigger_prototype.py
@@ -0,0 +1,13 @@
+from .trigger import *
+from . import _DiscoveryFlag, _EntityStatus, _Priority
+
+
+TriggerPrototypeFlag = _DiscoveryFlag
+TriggerPrototypeStatus = _EntityStatus
+TriggerPrototypePriority = _Priority
+TriggerPrototypeState = TriggerState
+TriggerPrototypeType = TriggerType
+TriggerPrototypeValue = TriggerValue
+TriggerPrototypeRecoveryMode = TriggerRecoveryMode
+TriggerPrototypeCorrelationMode = TriggerCorrelationMode
+TriggerPrototypeManualClose = TriggerManualClose

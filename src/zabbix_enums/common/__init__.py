8b9f13eac197fa57a2279e73999f43e8f8b09f8c
Robert Szulist <r.szulist@gmail.com>
2022-02-10 19:31:15 +0100

    Add custom enum

--- src/zabbix_enums/common/__init__.py
+++ src/zabbix_enums/common/__init__.py
@@ -1,3 +1,13 @@
+from enum import IntEnum
+
+
+class _ZabbixEnum(IntEnum):
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, str):
+            return cls(int(value))
+
+
 from .alert import *
 from .audit_log import *
 from .dashboard import *

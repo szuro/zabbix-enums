ec95c6bbdfc1cede469c50d008597c53abdbc9e8
Robert Szulist <r.szulist@gmail.com>
2024-08-31 19:21:58 +0200

    Add support for Zabbix 6.4

---
+++ src/zabbix_enums/z64/hostgroup.py
@@ -0,0 +1,13 @@
+"""https://www.zabbix.com/documentation/6.4/en/manual/api/reference/hostgroup/object"""
+from zabbix_enums import _ZabbixEnum
+
+
+class HostGroupFlag(_ZabbixEnum):
+    """
+    https://www.zabbix.com/documentation/6.4/en/manual/api/reference/hostgroup/object#host-group
+    
+    [Readonly]
+    Origin of the host group.
+    """
+    PLAIN = 0
+    DISCOVERED = 4

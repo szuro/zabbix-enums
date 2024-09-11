b99035164ca1d7ff871d04c6139af102a3234b9e
Robert Szulist <r.szulist@gmail.com>
2024-09-11 23:29:15 +0200

    Add support for Zabbix 7.0

---
+++ src/zabbix_enums/z70/mfa.py
@@ -0,0 +1,32 @@
+"""https://www.zabbix.com/documentation/7.0/en/manual/api/reference/mfa/object"""
+from zabbix_enums import _ZabbixEnum
+
+
+class MFAType(_ZabbixEnum):
+    """
+    https://www.zabbix.com/documentation/7.0/en/manual/api/reference/mfa/object#mfa
+    
+    Type of the MFA method.
+    """
+    TOPT = 1 
+    DUO = 2
+
+
+class MFAHashFunction(_ZabbixEnum):
+    """
+    https://www.zabbix.com/documentation/7.0/en/manual/api/reference/mfa/object#mfa
+    
+    Type of the hash function for generating TOTP codes.
+    """
+    SHA1 = 1
+    SHA256 = 2
+    SHA512 = 3
+
+class MFACodeLength(_ZabbixEnum):
+    """
+    https://www.zabbix.com/documentation/7.0/en/manual/api/reference/mfa/object#mfa
+    
+    Verification code length.
+    """
+    SIX = 6
+    EIGHT = 8
